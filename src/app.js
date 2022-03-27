6dc8905e9e0e6160e98737f7ec64f679e0eef855
Matheus Amorim <matheus.amorim02@gmail.com>
2022-03-26 23:33:10 -0300

    Criação do CRUD

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const livros = [
     {id: 1, "titulo": "Senhor dos Aneis" },
     {id: 2, "titulo": "O Hobbit" }
@@ -12,7 +14,34 @@ app.get('/', (req, res) => {
 })
 
 app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
+    res.status(200).json(livros);
 })
+
+app.get("/livros/:id", (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
+app.post('/livros', (req, res) =>{
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso')
+})
+
+app.put("/livros/:id", (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros)
+})
+
+app.delete("/livros/:id", (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1);
+    res.send(`Livro ${id} removido com sucesso`)
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livros => livros.id == id);
+}
 
 export default app
